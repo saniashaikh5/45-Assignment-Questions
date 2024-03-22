60f788b434bc4d695749f31ff67075068839839d
saniashaikh5 <160105353+saniashaikh5@users.noreply.github.com>
2024-03-22 06:25:18 -0700

    Add files via upload

---
+++ 30/main.js
@@ -0,0 +1,2 @@
+var username = "aliya,Admin, maria,taha,zain";
+console.log("Hello ".concat(username + 1, " would you like to see a status report?"));
---
+++ 30/main.ts
@@ -0,0 +1,2 @@
+let username = "aliya,Admin, maria,taha,zain";
+console.log(`Hello ${username+1} would you like to see a status report?`)
