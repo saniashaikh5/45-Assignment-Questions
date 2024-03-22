c3ab31cf3250be7690b59643d9150a80cbd330d1
saniashaikh5 <160105353+saniashaikh5@users.noreply.github.com>
2024-03-22 06:22:07 -0700

    Add files via upload

---
+++ 25. Alien Color/main.js
@@ -0,0 +1,9 @@
+// let alien_color = "green";
+// if (alien_color == "green"){
+//     console.log("you just earnd 5 points!");
+// }
+//verson that fails 
+var alien_color = "red";
+if (alien_color == "green") {
+    console.log("you just earnd 5 points");
+}
---
+++ 25. Alien Color/main.ts
@@ -0,0 +1,11 @@
+// let alien_color = "green";
+// if (alien_color == "green"){
+    // console.log("you just earnd 5 points!");
+// }
+
+//verson that fails 
+
+let alien_color = "red";
+if (alien_color == "green"){
+    console.log("you just earnd 5 points")
+}
