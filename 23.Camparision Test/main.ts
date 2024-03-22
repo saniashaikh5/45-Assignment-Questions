ab34f1cfac59290a7de8cdf3423c72a03a7d6e8f
saniashaikh5 <160105353+saniashaikh5@users.noreply.github.com>
2024-03-22 06:20:53 -0700

    Add files via upload

---
+++ 23.Camparision Test/main.ts
@@ -0,0 +1,46 @@
+let car: string = "sabaru";
+console.log("is car == 'sabaru'? i predict true.");
+console.log(car == 'sabaru');
+
+ //test 2 
+let subject: string = 'English'
+console.log("is subject == 'englis'? i predict false.");
+console.log(subject == 'english');
+
+//test 3 
+let ball: string = "red";
+console.log("is ball == 'red'? i predict true.");
+console.log(ball == 'red');
+
+//test 4 
+let language: string = "English";
+console.log("is language == 'English'? i predict true");
+console.log(language == 'English');
+
+ //test 5
+
+
+// //test 6
+let subject: string = "math";
+console.log("is subject == 'math'? i predict false");
+console.log(subject == 'math');
+
+// //test 7 
+let city: string = "karachi";
+console.log("is city == 'karachi'? i predict true");
+console.log(city == 'karachi');
+
+//test 8
+let camera: string = "Yellow"
+console.log("is camera == 'yellow'? i predict false.");
+console.log(camera == 'yellow');
+
+// //test 9
+let cat: string = "Blue"
+console.log("is cat == 'blue'? i predict false");
+console.log(cat == 'blue');
+
+//test 10
+let Butterfly: string = "pink";
+console.log("is Butterfly == 'pink'? i predict true.");
+console.log(Butterfly == 'pink');
