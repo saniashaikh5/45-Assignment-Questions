90d92ac194229612896c380de6ce1666a927c205
saniashaikh5 <160105353+saniashaikh5@users.noreply.github.com>
2024-03-22 06:16:13 -0700

    Add files via upload

---
+++ 14.Guest List/app.js
@@ -0,0 +1,28 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
+var guest_list = ['ali', 'farhan', 'lisa', 'raniya', 'muskan'];
+exports.guest_list = guest_list;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log('Respected Sir/Madam' + guest_list[i] + ',/nWe invited you on dinner tommorow.\nThank You');
+}
+//exercise 15 
+// let not_present : string = 'farhan';
+// let new_guest : string = 'saad';
+guest_list[1] = new_guest;
+for(let i=0; i<guest_list.length; i++){
+   console.log('Respected Sir/Madam' + guest_list[i] + ',/nWE invited you on dinner tommorow.\nThank You')
+}
+//Q 16
+// guest_list.unshift('shaheen','rizwan','sarfaraz');
+//Q 17
+// console.log('\nunfortunately we can not arrange big table, only two people allow.')
+// while(guest_list.length>2){
+// let remove_guest = guest_list.pop();
+// console.log(`sorry Sir/Madam ${remove_guest} you not invited for dinner.`);
+// }
+// for(let i=0; i<guest_list.length; i++){
+//    console.log('Respected Sir/Madam' + guest_list[i] + ',/nWE invited you on dinner tommorow.\nThank You')
+// }
+// guest_list.splice(0,2)
+// console.log(guest_list)
---
+++ 14.Guest List/app.ts
@@ -0,0 +1,29 @@
+let guest_list : string [] = ['ali','farhan','lisa','raniya','muskan'];
+ for(let i=0; i<guest_list.length; i++){
+    console.log('Respected Sir/Madam' + guest_list[i] + ',/nWe invited you on dinner tommorow.\nThank You')
+ }
+   export{guest_list}
+
+//exercise 15 
+
+let not_present : string = 'farhan';
+let new_guest : string = 'saad';
+guest_list[1] = new_guest;
+for(let i=0; i<guest_list.length; i++){
+   console.log('Respected Sir/Madam' + guest_list[i] + ',/nWE invited you on dinner tommorow.\nThank You')
+}
+//Q 16
+
+guest_list.unshift('shaheen','rizwan','sarfaraz');
+
+//Q 17
+console.log('\nunfortunately we can not arrange big table, only two people allow.')
+while(guest_list.length>2){
+let remove_guest = guest_list.pop();
+console.log(`sorry Sir/Madam ${remove_guest} you not invited for dinner.`);
+}
+for(let i=0; i<guest_list.length; i++){
+   console.log('Respected Sir/Madam' + guest_list[i] + ',/nWE invited you on dinner tommorow.\nThank You')
+}
+guest_list.splice(0,2)
+console.log(guest_list)
