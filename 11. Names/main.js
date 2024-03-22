d695470f436099da0c86932588c29ddc8b042cb1
saniashaikh5 <160105353+saniashaikh5@users.noreply.github.com>
2024-03-22 06:06:52 -0700

    Add files via upload

---
+++ 11. Names/main.js
@@ -0,0 +1,10 @@
+var members = ['shifa', 'sana', 'unsa', 'haniya', 'zainab',];
+for (var i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
+//excercise 12
+var members = ['shifa', 'sana', 'unsa', 'haniya', 'zainab',];
+var message = 'whose birthday tommorow';
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ 11. Names/main.ts
@@ -0,0 +1,13 @@
+let members: string[] =['shifa','sana','unsa','haniya','zainab',];
+for(let i=0; i<members.length; i++){
+    console.log(members[i]);
+}
+
+//excercise 12
+
+let members: string[] =['shifa','sana','unsa','haniya','zainab',];
+let message: string= 'whose birthday tommorow'
+for(let i=0; i<members.length; i++){
+    console.log(message + members[i]);
+}
+
