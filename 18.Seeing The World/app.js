d477c5406e78b1b54d62e2f038fa748eae0475dd
saniashaikh5 <160105353+saniashaikh5@users.noreply.github.com>
2024-03-22 06:17:47 -0700

    Add files via upload

---
+++ 18.Seeing The World/app.js
@@ -0,0 +1,18 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var places = ['cape town', 'delhi', 'bankok', 'faisalabad', 'ahmadabad'];
+console.log('original ' + places);
+console.log('copy ' + __spreadArray([], places, true).sort());
+console.log('original ' + places);
+console.log('copy ' + __spreadArray([], places, true).sort().reverse());
+console.log('copy ' + __spreadArray([], places, true).sort().reverse());
+console.log('original ' + places);
+console.log('original ' + places.reverse());
+console.log('change the ' + places);
---
+++ 18.Seeing The World/app.ts
@@ -0,0 +1,15 @@
+let places : string [] = ['cape town','delhi','bankok','faisalabad','ahmadabad']
+ console.log('original ' + places);
+
+ console.log('copy ' + [...places].sort());
+
+ console.log('original ' + places);
+
+console.log('copy ' + [...places].sort().reverse());
+
+console.log('copy ' + [...places].sort().reverse());
+
+console.log('original ' + places);
+
+console.log('original ' + places.reverse());
+console.log('change the ' + places );
