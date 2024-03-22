67357468083848a3cc7c1bac43725678820b8a97
saniashaikh5 <160105353+saniashaikh5@users.noreply.github.com>
2024-03-22 06:08:01 -0700

    Add files via upload

---
+++ 6.Stripping Names/app.js
@@ -0,0 +1,4 @@
+var personName = "\n\t SANIA SHEIKH\t\n";
+console.log(personName);
+var stripped = personName.trim();
+console.log(stripped);
---
+++ 6.Stripping Names/app.ts
@@ -0,0 +1,4 @@
+let personName: string =`\n\t SANIA SHEIKH\t\n`;
+console.log(personName);
+let stripped: string = personName.trim();
+console.log(stripped); 
